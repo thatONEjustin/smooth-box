35c8ef6d2e90877f954cf66b78f0d54cf1b8bde9
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-07-06 13:04:37 -0500

    waybar changes

--- custom/waybar/postcss.config.js
+++ custom/waybar/postcss.config.js
@@ -2,8 +2,7 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-nesting': {},
-    // 'tailwindcss/nesting': {},
-    // tailwindcss: {},
+    'postcss-simple-vars': {},
     autoprefixer: {},
   }
 }
