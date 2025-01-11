866f52ebfefc2bf15d123038003f911062aeef3e
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2025-01-11 10:54:55 -0600

    learning bash better
    looping through an array!

--- first_run.sh
+++ first_run.sh
@@ -2,6 +2,20 @@
 # first_run.sh
 #
 # Working on my first install script!
+#
 
-mkdir -p $HOME/work
-mkdir -p $HOME/github
+personal_directories=("work" "github")
+
+for i in "${personal_directories[@]}"; do
+  if [ ! -d "$HOME/$i" ]; then
+    mkdir -p $HOME/work
+  fi
+done
+
+# if [ -d "$HOME/work" ]; then
+#   mkdir -p $HOME/work
+# fi
+#
+# if [ -d "$HOME/work" ]; then
+#   mkdir -p $HOME/github
+# fi
