38755a1ec2c140abaf2dc7eb9299e670b5e9c949
Justin P <2763180+thatONEjustin@users.noreply.github.com>
2025-01-20 15:50:18 -0600

    kde eject button
    - deletes customizations and themes
    - does not fix sddm dependency errors

---
+++ kde_reset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/
+rm -rf .kde
+rm -rf .cache/plasmashell*
+rm -rf .cache/org.kde.dirmodel-qml.kcache
+rm -rf .cache/kioexec/ .cache/krunner/ .cache/ksycoca5*
+rm -rf .cache/krunnerbookmarkrunnerfirefoxdbfile.sqlite
+rm -rf .config/plasma*
+rm -rf .config/kde*
