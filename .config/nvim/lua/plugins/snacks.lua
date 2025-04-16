6385adab1d00abf3e5e832f1a2aa6edb689d63d5
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2025-04-16 13:07:44 -0500

    snacks update
    - remove base explorer from defaults

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    explorer = { enabled = false },
+  },
+}
