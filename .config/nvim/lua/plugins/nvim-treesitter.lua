91b2bf8e23d8fd33a420481716e5c5abb916bca7
Wojojustin <42546444+WojoJustin@users.noreply.github.com>
2025-05-09 16:28:36 -0500

    removing custom blade support
    unneeded, there needs to be a ACF Sage Directives LSP/grammar setup but
    i don't know how to do that. I guess learn it now lol

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -47,24 +47,24 @@ return {
       "yaml",
     },
   },
-  config = function(_, opts)
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-      filetype = "blade",
-    }
-
-    vim.filetype.add({
-      pattern = {
-        [".*%.blade%.php"] = "blade",
-      },
-    })
-
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+  -- config = function(_, opts)
+  --   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  --
+  --   parser_config.blade = {
+  --     install_info = {
+  --       url = "https://github.com/EmranMR/tree-sitter-blade",
+  --       files = { "src/parser.c" },
+  --       branch = "main",
+  --     },
+  --     filetype = "blade",
+  --   }
+  --
+  --   vim.filetype.add({
+  --     pattern = {
+  --       [".*%.blade%.php"] = "blade",
+  --     },
+  --   })
+  --
+  --   require("nvim-treesitter.configs").setup(opts)
+  -- end,
 }
