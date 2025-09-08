fc01c5b1ffbb65b5650094e43bc07eb80e329e91
Wojojustin <42546444+WojoJustin@users.noreply.github.com>
2025-09-08 13:04:18 -0500

    snacks and stuff

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,7 +5,8 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    explorer = { enabled = true },
+    explorer = { enabled = false },
+    dashboard = { enabled = false },
     -- picker.sources.explorer.win.list.wo.relativenumber = true,
     picker = {
       hidden = true,
