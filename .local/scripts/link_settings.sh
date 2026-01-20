2ee47a627be5cf049c50daa9f6f796f43cdd3b4c
justinp <2763180+thatONEjustin@users.noreply.github.com>
2026-01-20 16:55:11 -0600

    link settings
    now properly updates omz

--- .local/scripts/link_settings.sh
+++ .local/scripts/link_settings.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
+rm -rf ~/.config/pavucontrol.ini
+wait
 rm -rf ~/.oh-my-zsh/custom
-ln -s $HOME/github/smooth-box/.oh-my-zsh/custom ~/.oh-my-zsh/custom
+wait
+zsh "$ZSH/tools/upgrade.sh"
+wait
 
 cd $smooth/
 stow --target="$HOME" --dir="$HOME/github/smooth-box" .
