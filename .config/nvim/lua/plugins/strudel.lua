836ec8166211db9065f1d41d282becfe1ea7fb85
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2026-01-20 15:57:02 -0600

    strudel

--- .config/nvim/lua/plugins/strudel.lua
+++ .config/nvim/lua/plugins/strudel.lua
@@ -1,13 +1,9 @@
 return {
-  {
-    "gruvw/strudel.nvim",
-    build = "npm install",
-    config = function()
-      require("strudel").setup({
-        -- headless = true,
-        update_on_save = true,
-        -- browser_exec_path = "/usr/bin/zen-browser",
-      })
-    end,
-  },
+  "gruvw/strudel.nvim",
+  build = "npm install",
+  config = function()
+    require("strudel").setup({
+      update_on_save = false,
+    })
+  end,
 }
