0840e7288336d29b9948ad77218482eb7aeea772
Justin P <2763180+thatONEjustin@users.noreply.github.com>
2026-01-26 16:15:30 -0600

    niri!

--- .local/scripts/launch_terminal.sh
+++ .local/scripts/launch_terminal.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# ghostty
+tmux_script=$HOME/.local/scripts/tmux_attach.sh
 
-if tmux info &>/dev/null; then
-    wezterm
+if [ ! $(pgrep niri &>/dev/null) ]; then
+    terminal=ghostty
+    term_tmux="ghostty -e ${tmux_script}"
 else
-    wezterm start -- $HOME/.local/scripts/tmux_attach.sh
+    terminal=wezterm
+    term_tmux="wezterm start -- $HOME/.local/scripts/tmux_attach.sh"
+fi
+
+if [ ! $(tmux info &>/dev/null) ]; then
+    ${terminal}
+else
+    ${term_tmux}
 fi
